d8ab5f089d26e8b884deff671839d50a04b05bf0
mqm1999 <61899832+mqm1999@users.noreply.github.com>
2020-08-02 13:08:57 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const pool = require('./utils/db');
 const app = express();
 
+app.get('/', (req, res) => {
+    pool.query('select * from `user`;', (err, data) => {
+        if (err) {
+            res.send('Error query')
+        }
+        console.log(data);
+        res.send(data)
+    })
+});
+
 app.get('/user', (req, res) => {
     pool.query('select * from `user`;', (err, data) => {
         if (err) {
